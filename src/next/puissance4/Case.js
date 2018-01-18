6fec5c49c46f3a4e1426deb2e6706f0bd28794f0
Charles Jourdan <charles.jourdan@protonmail.com>
2018-01-18 11:04:03 +0000

    Suppression des doublons et ajout du meta charset UTF-8

--- src/next/puissance4/Case.js
+++
@@ -1,7 +0,0 @@
-export class Case {
-	constructor(line, column, jeton) {
-		this.line = line;
-		this.column = column;
-		this.jeton = jeton;
-	}
-}
--- src/next/puissance4/Jeton.js
+++
@@ -1,5 +0,0 @@
-export class Jeton {
-	constructor(value) {
-		this.value = value;
-	}
-}
